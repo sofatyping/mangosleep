52cab26bad1e92d721820d80a57b4bdafb5a50ba
Yu <yuhsia89@gmail.com>
2018-09-05 23:28:18 -0700

    Spring project setup

---
+++ src/main/java/com/project/mango/mangosleep/MangosleepApplication.java
@@ -0,0 +1,12 @@
+package com.project.mango.mangosleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangosleepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangosleepApplication.class, args);
+	}
+}
